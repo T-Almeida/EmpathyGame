d88cac15bea3e75be7e62224650d702fce3ba145
T-Almeida <tiagomeloalmeida@ua.pt>
2020-05-01 16:43:39 +0100

    Updates

---
+++ precache-manifest.98e4287e8d2e047b7047ff56ca560c92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd61f08a1b87e8d06fa3a72e13b9c7e",
+    "url": "/empathy-game/index.html"
+  },
+  {
+    "revision": "9333bbbcce42c1a3c10b",
+    "url": "/empathy-game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e79e15c411a189d9a1b1",
+    "url": "/empathy-game/static/js/2.c1c2bca8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/empathy-game/static/js/2.c1c2bca8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9333bbbcce42c1a3c10b",
+    "url": "/empathy-game/static/js/main.0188ca6f.chunk.js"
+  },
+  {
+    "revision": "238ab30e65de05162df4",
+    "url": "/empathy-game/static/js/runtime-main.0734816d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/empathy-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
