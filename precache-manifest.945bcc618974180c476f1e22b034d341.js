3fd55034c5afa154dd521908a29ff39d6bcb7571
T-Almeida <tiagoalmeida.6.8.1996@gmail.com>
2020-07-08 12:47:51 +0000

    Deploying to gh-pages from  @ 9524cd861d11f2e6829a48766e1ab17f85170caf 🚀

---
+++ precache-manifest.945bcc618974180c476f1e22b034d341.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b228902d7d4b39931ad2a4dcef72a666",
+    "url": "/empathy-game/index.html"
+  },
+  {
+    "revision": "85bc51f80a71b35bff0d",
+    "url": "/empathy-game/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "a54e77dd00d9a82e5e2d",
+    "url": "/empathy-game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "85bc51f80a71b35bff0d",
+    "url": "/empathy-game/static/js/2.6160b158.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/empathy-game/static/js/2.6160b158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a54e77dd00d9a82e5e2d",
+    "url": "/empathy-game/static/js/main.39c6b56e.chunk.js"
+  },
+  {
+    "revision": "238ab30e65de05162df4",
+    "url": "/empathy-game/static/js/runtime-main.0734816d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/empathy-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
